fc92de435574daf43b2ce04e075c94e7a80aec80
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2019-11-21 09:35:03 -0300

    Recategoriza despesas e compara partidos grandes

--- code/lib.R
+++ code/lib.R
@@ -102,50 +102,55 @@ read_projectdata <- function(){
                     ) )
 }
 
+
+unwanted_array = list(    'Š'='S', 'š'='s', 'Ž'='Z', 'ž'='z', 'À'='A', 'Á'='A', 'Â'='A', 'Ã'='A', 'Ä'='A', 'Å'='A', 'Æ'='A', 'Ç'='C', 'È'='E', 'É'='E',
+                          'Ê'='E', 'Ë'='E', 'Ì'='I', 'Í'='I', 'Î'='I', 'Ï'='I', 'Ñ'='N', 'Ò'='O', 'Ó'='O', 'Ô'='O', 'Õ'='O', 'Ö'='O', 'Ø'='O', 'Ù'='U',
+                          'Ú'='U', 'Û'='U', 'Ü'='U', 'Ý'='Y', 'Þ'='B', 'ß'='Ss', 'à'='a', 'á'='a', 'â'='a', 'ã'='a', 'ä'='a', 'å'='a', 'æ'='a', 'ç'='c',
+                          'è'='e', 'é'='e', 'ê'='e', 'ë'='e', 'ì'='i', 'í'='i', 'î'='i', 'ï'='i', 'ð'='o', 'ñ'='n', 'ò'='o', 'ó'='o', 'ô'='o', 'õ'='o',
+                          'ö'='o', 'ø'='o', 'ù'='u', 'ú'='u', 'û'='u', 'ý'='y', 'ý'='y', 'þ'='b', 'ÿ'='y' )
+
+lower_ascii <- function(s){
+    chartr(paste(names(unwanted_array), collapse=''),
+           paste(unwanted_array, collapse=''),
+           str_to_lower(s))
+}
+
 read_dados_issa2018 <- function(){
     r <- function(f){
-        read_csv(here::here("data/categorias/", f)) %>% pull(1) 
+        read_csv2(here::here("data/categorias/", f), col_types = "c") %>% pull(1) %>% str_trim()
     }
     rt <- function(f){
-        read_csv(here::here("data/categorias/", f)) %>% names()
+        read_csv2(here::here("data/categorias/", f), col_types = "c") %>% names() %>% str_trim()
     }
-    read_csv2(here::here("data/raw/SPCA 2018.csv"),
+    recategoriza <- function(df, f) {
+        df %>%
+            mutate(categoria = if_else(
+                ds_gasto_match %in% lower_ascii(r(f)),
+                rt(f),
+                categoria
+            ))
+    }
+    
+    d = read_csv2(here::here("data/raw/SPCA 2018.csv"),
               locale = locale(encoding = "latin1"), 
               col_types = "ccccc") %>%
         mutate(
             valor = stringr::str_replace_all(VR_GASTO, "R\\$ |\\.", "") %>% str_replace(",", ".") %>% as.double(),
-            DS_GASTO = str_to_title(DS_GASTO)
-        ) %>% 
-        mutate(categoria = case_when(
-            DS_GASTO %in% c("Despesas Com Publicidade Por Adesivos - Eleitorais") ~ "Adesivos",
-            DS_GASTO %in% c("Ativo Permanente - Bens Imóveis - Edificações", 
-                            "Ativo Permanente - Bens Imóveis - Instalações",
-                            "Ativo Permanente - Bens Imóveis - Outros Bens Imóveis") ~ "Aquisição e edificação de imóveis",
-            DS_GASTO %in% c("Transportes E Viagens - Combustíveis, Óleos E Lubrificantes - Despesas Eleitorais",  
-                            "Transportes E Viagens - Combustíveis, Óleos E Lubrificantes - Mulheres",
-                            "Transportes E Viagens - Combustíveis, Óleos E Lubrificantes - Ordinárias"
-            ) ~ "Combustível",
-            DS_GASTO %in% c("Comícios E Militância De Rua - Despesas Eleitorais",
-                            "Despesas Com Publicidade Por Carros De Som - Eleitorais"
-            ) ~ "Comícios, carros de som e militância de rua",
-            DS_GASTO %in% c("Congressos - Mulheres", 
-                            "Congressos - Ordinárias",  
-                            "Convenções - Mulheres", 
-                            "Convenções Eleitorais - Despesas Eleitorais",  
-                            "Convenções Partidárias - Ordinárias", 
-                            "Despesas Com Convenções Partidárias - Eleitorais",  
-                            "Despesas Com Eventos De Promoção De Candidaturas - Eleitorai",
-                            "Eventos Promocionais - Despesas Eleitorais ", 
-                            "Eventos Promocionais - Mulheres", 
-                            "Eventos Promocionais - Ordinárias",  
-                            "Instalação De Comitês Financeiros E De Campanha - Despesas Eleitorais", 
-                            "Seminários - Mulheres",  
-                            "Seminários - Ordinárias"
-            ) ~ "Congressos, convenções, seminários e eventos",
-            DS_GASTO %in% c("Serviços Técnico-profissionais - Serviços De Consultoria Jurídica - Despesas Eleitorais") ~ "Consultoria jurídica - eleitoral",
-            DS_GASTO %in% c(r("uma")~ rt("uma")) %>% names(),
-            DS_GASTO %in% c() ~ "",
-            DS_GASTO %in% c() ~ "",
-            DS_GASTO %in% c() ~ "",
-        ))
+            DS_GASTO = str_to_title(DS_GASTO),
+            categoria = NA_character_,
+            ds_gasto_match = lower_ascii(DS_GASTO) %>% str_trim()
+        ) 
+    
+    for (i in 1:51) {
+        d = d %>% recategoriza(paste0("categoria-", i, ".txt"))
+    }
+    
+    # TODO -> Por que essa categoria dá errado? 
+    d = d %>% mutate(categoria = if_else(is.na(categoria) & 
+                                             str_starts(DS_GASTO, "Despesas Gerais - Mensalidades, Anuidades E Congêneres Devidos A Organismos Partidários Internacionais"),
+                                         "Mensalidades para organismos internacionais", categoria))
+    
+    d = d %>% mutate(categoria = if_else(is.na(categoria), "Não categorizadas", categoria))
+    
+    return(d)
 }
